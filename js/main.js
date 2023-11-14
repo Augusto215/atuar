b4c9d7fd12289f87cd00878a3447877bbb09ff7a
Augusto215 <augusto.dataanalysis@gmail.com>
2023-11-14 05:52:29 -0300

    novo

--- js/main.js
+++ js/main.js
@@ -270,6 +270,30 @@ $(document).ready(function(){
 
 
 
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		margin: 10,
+		responsiveClass: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		nav: true, // Habilita a navegação
+		navText: [
+			"<i class='fa fa-chevron-left'></i>",
+			"<i class='fa fa-chevron-right'></i>"
+		],
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			1000: {
+				items: 4
+			}
+		}
+	});
+
 
 
 
